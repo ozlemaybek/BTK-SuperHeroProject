b7e0f0cf270a42db9cd75836b780fe70942d5be7
Özlem Katibe AYBEK <ozlemkatibeaybek@gmail.com>
2022-07-27 16:06:11 +0300

    Project updated.

--- app/src/main/java/com/ozlem/superheroproject/MainActivity.kt
+++ app/src/main/java/com/ozlem/superheroproject/MainActivity.kt
@@ -2,10 +2,44 @@ package com.ozlem.superheroproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+// MainActivity ve XML arasındaki senkronizasyonu sağlamak için:
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Bir değişkene her yerden ulaşmak istiyorsam burada tanımlamalıyım (global)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickCreateSuperHero(view : View){
+
+        // toString metodu sonucu String'e çevirmek için kullanılır:
+        // Girilen inputları string'e çevirip değişkenlere atayalım:
+        val name = plainTextNameID.text.toString()
+        // Primary constructor age parametresini integer olarak isteyeceği için çevirmeliyiz.
+        // Fakat kullanıcı String girerse uygulama çöker bu yüzden toInt() değil toIntOrNull() metodu kullanalım:
+        // Bu işlemi yaptığım takdirde age artık bir Int değil IntNullable
+        // Bu yüzden aşağıda age parametresi null değer olabileceğinden hata verir.
+        // Bunu bir null safety mekanizması ile düzeltebiliriz.
+        val age = plainTextAgeID.text.toString().toIntOrNull()
+        val job = plainTextJobID.text.toString()
+
+        //null safety:
+        if(age == null){
+            textViewID.text = "Please enter the correct age..."
+        }else{
+            // Şimdi oluşturduğumuz SuperHero sınıfını kullanarak işlemler yapalım:
+            val superHero = SuperHero(name,age!!,job)
+            // Yukarıdaki satırda !! koymayabilirim.
+            // Çünkü kotlin bir güvenlik mekanizmasında olduğunu ve artık age değerinin null olmayacağını anlıyor.
+
+            // Girilen inputları textView'da bastırdık:
+            textViewID.text = "name: ${superHero.name} , age: ${superHero.age} , job: ${superHero.job}"
+        }
+
     }
 }
---
+++ app/src/main/java/com/ozlem/superheroproject/SuperHero.kt
@@ -0,0 +1,6 @@
+package com.ozlem.superheroproject
+
+// Primary constructor = Class:
+class SuperHero (var name : String , var age : Int , var job : String){
+
+}
